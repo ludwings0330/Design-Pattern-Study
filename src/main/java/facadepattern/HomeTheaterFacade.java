b504cb7a9a7082e58f5d6fb7df969251a4081cca
ludwings <ludwings0330@gmail.com>
2022-02-20 21:30:41 +0900

    [Design Pattern] Facade Pattern Code

---
+++ src/main/java/facadepattern/HomeTheaterFacade.java
@@ -0,0 +1,52 @@
+package facadepattern;
+
+public class HomeTheaterFacade {
+    Amplifier amp;
+    Tuner tuner;
+    DvdPlayer dvd;
+    CdPlayer cd;
+    Projector projector;
+    TheaterLights lights;
+    Screen screen;
+    PopcornPopper popper;
+
+    public HomeTheaterFacade(Amplifier amp, Tuner tuner,
+                             DvdPlayer dvd, CdPlayer cd, Projector projector,
+                             TheaterLights lights, Screen screen, PopcornPopper popper) {
+
+        this.amp = amp;
+        this.tuner = tuner;
+        this.dvd = dvd;
+        this.cd = cd;
+        this.projector = projector;
+        this.lights = lights;
+        this.screen = screen;
+        this.popper = popper;
+    }
+
+    public void watchMovie(String movie) {
+        System.out.println("HomeTheaterFacade.watchMovie");
+        popper.on();
+        popper.pop();
+        lights.dim(10);
+        screen.down();
+        projector.on();
+        projector.wideScreenMode();
+        amp.on();
+        amp.setDvd(dvd);
+        dvd.on();
+        dvd.play(movie);
+    }
+
+    public void endMovie() {
+        System.out.println("HomeTheaterFacade.endMovie");
+        popper.off();
+        lights.on();
+        screen.up();
+        projector.off();
+        amp.off();
+        dvd.stop();
+        dvd.eject();
+        dvd.off();
+    }
+}
